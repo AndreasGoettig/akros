60f27f625e52d024763b8efef86bd24994769959
AndreasG <Goettig.Andreas@gmx.de>
2021-09-07 21:21:43 +0200

    multiplayer stuff

---
+++ Assets/Resources/Animations/testAnimationManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testAnimationManager : MonoBehaviour
+{
+    public Animator animator;
+
+    [SerializeField]
+    private float directionDampTime = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        // only allow jumping if we are running.
+        if (stateInfo.IsName("Base Layer.Run"))
+        {
+            // When using trigger parameter
+            if (Input.GetButtonDown("Jump"))
+            {
+                animator.SetTrigger("Jump");
+            }
+        }
+
+        float h = Input.GetAxis("Horiontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v < 0)
+        {
+            v = 0;
+        }
+        animator.SetFloat("Speed", h * h + v * v);
+        animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+    }
+}
